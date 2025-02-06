9638add9c21df0f64d4e514509dc819f6c09f1b3
kaustubhchandra zarkar <73283594+kaustubhchandra@users.noreply.github.com>
2025-02-07 04:32:43 +0530

    Update variable.tf

--- tf-modules/gke-cluster/variable.tf
+++ tf-modules/gke-cluster/variable.tf
@@ -1,5 +1,35 @@
-variable "region" {
+variable "project" {
+  description = "The Google Cloud project to use"
   type        = string
-  description = "The Google Cloud region"
+  default     = "asysops"
+}
+
+variable "region" {
+  description = "The region for the GKE cluster"
+  type        = string
   default     = "asia-south1"
+}
+
+variable "zone" {
+  description = "The zone to create the GKE cluster"
+  type        = string
+  default     = "asia-south1-b"
+}
+
+variable "machine_type" {
+  description = "Machine type for the GKE nodes"
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "min_node_count" {
+  description = "Minimum node count for the GKE cluster autoscaler"
+  type        = number
+  default     = 2
+}
+
+variable "max_node_count" {
+  description = "Maximum node count for the GKE cluster autoscaler"
+  type        = number
+  default     = 5
 }
