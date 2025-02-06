f0573b7380324ad9eeadfcb38c12b9104672149d
kaustubhchandra zarkar <73283594+kaustubhchandra@users.noreply.github.com>
2025-02-07 04:23:36 +0530

    Update variable.tf

--- tf-modules/gke-cluster/variable.tf
+++ tf-modules/gke-cluster/variable.tf
@@ -1,8 +1,3 @@
-variable "project_id" {
-  type        = string
-  description = "The Google Cloud Project ID"
-}
-
 variable "region" {
   type        = string
   description = "The Google Cloud region"
