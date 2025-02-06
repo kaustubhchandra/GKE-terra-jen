7e1f5c3cc94a0fab1856f6b6ed3abedc58c0cd65
kaustubhchandra zarkar <73283594+kaustubhchandra@users.noreply.github.com>
2025-02-07 04:34:54 +0530

    Update main.tf

--- tf-modules/gke-cluster/main.tf
+++ tf-modules/gke-cluster/main.tf
@@ -1,25 +1,36 @@
 provider "google" {
-  project = "asysops"
-  region  = "asia-south1"
+  project = var.project
+  region  = var.region
 }
 
 resource "google_container_cluster" "primary" {
   name     = "highway-cluster"
-  location = "asia-south1-b"
-}
+  location = var.zone
 
   initial_node_count = 2
+
   node_config {
-    machine_type = "e2-medium"
+    machine_type = var.machine_type
   }
 
   enable_autoscaling {
-    min_node_count = 2
-    max_node_count = 5
+    min_node_count = var.min_node_count
+    max_node_count = var.max_node_count
   }
 
-output "cluster_endpoint" {
-  value = google_container_cluster.primary.endpoint
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  cluster    = google_container_cluster.primary.name
+  location   = var.zone
+  node_count = 2
+
+  node_config {
+    machine_type = var.machine_type
+  }
+
+  autoscaling {
+    min_node_count = var.min_node_count
+    max_node_count = var.max_node_count
+  }
 }
-
-
