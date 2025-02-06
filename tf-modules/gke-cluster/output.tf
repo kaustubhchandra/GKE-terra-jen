c4a02a33f214c3f61a01cadc34a7e814919b7b9a
kaustubhchandra zarkar <73283594+kaustubhchandra@users.noreply.github.com>
2025-02-07 04:33:33 +0530

    Update output.tf

--- tf-modules/gke-cluster/output.tf
+++ tf-modules/gke-cluster/output.tf
@@ -1,3 +1,9 @@
 output "cluster_name" {
-  value = google_container_cluster.primary.name
+  description = "The name of the GKE cluster"
+  value       = google_container_cluster.primary.name
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint of the GKE cluster"
+  value       = google_container_cluster.primary.endpoint
 }
